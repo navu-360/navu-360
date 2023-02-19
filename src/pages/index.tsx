befdf031920194ede2c1f53828bf26ac32e07da5
Liplan Lekipising <leeplanke@gmail.com>
2023-02-20 00:43:24 +0200

    chore: use Next Font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,13 @@ import React, { useState } from "react";
 import { useJoinWaitlistMutation } from "services/baseApiSlice";
 import toast from "utils/toast";
 
+import { Nunito_Sans } from "@next/font/google";
+
+const font = Nunito_Sans({
+  subsets: ["cyrillic"],
+  weight: ["400", "600", "700", "800"],
+});
+
 export default function Home() {
   const [joinWaitlist, { isLoading }] = useJoinWaitlistMutation();
 
@@ -29,7 +36,9 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-center">
+      <main
+        className={`flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-center ${font.className}`}
+      >
         <div className="flex flex-col items-center justify-center gap-6 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Navu <span className="text-[hsl(280,100%,70%)]">360</span>
@@ -37,12 +46,12 @@ export default function Home() {
           <div className="mx-auto md:w-max">
             <div className="mx-auto flex w-full flex-col gap-4 rounded-xl p-4 text-white md:w-1/2">
               <h2 className="text-2xl font-bold">Launching Soon!</h2>
-              <div className="text-md font-medium">
+              <div className="text-md">
                 Join Navu360&apos;s waitlist now and be among the first to
                 revolutionize your company&apos;s onboarding experience, keeping
                 your top talents and driving growth like never before!
               </div>
-              <form className="mx-auto w-full flex max-w-xs flex-col gap-4">
+              <form className="mx-auto flex w-full max-w-xs flex-col gap-4">
                 <input
                   type="email"
                   name="email"
