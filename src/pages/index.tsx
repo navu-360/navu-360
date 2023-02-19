7ad30d94c43267f8bec5e984ef708e4c08d0f18b
Liplan Lekipising <leeplanke@gmail.com>
2023-02-19 23:38:14 +0200

    fix: fix mobile responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,11 +30,11 @@ export default function Home() {
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-center">
-        <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16 ">
+        <div className="flex flex-col items-center justify-center gap-6 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Navu <span className="text-[hsl(280,100%,70%)]">360</span>
           </h1>
-          <div className="mx-auto w-max">
+          <div className="mx-auto md:w-max">
             <div className="mx-auto flex w-full flex-col gap-4 rounded-xl p-4 text-white md:w-1/2">
               <h3 className="text-2xl font-bold">Launching Soon!</h3>
               <div className="text-md font-medium">
