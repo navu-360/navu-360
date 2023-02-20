14c5df13da0c7a39ec3fd3bd54261cbbfffee08f
Liplan Lekipising <leeplanke@gmail.com>
2023-02-20 12:34:35 +0200

    chore: remove persist gate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { makeStore, persistor } from "redux/store";
+import { makeStore } from "redux/store";
 
 import { ToastContainer, Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -27,26 +26,24 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <ErrorBoundary>
       <SessionProvider session={session}>
         <Provider store={makeStore}>
-          <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
-            <ToastContainer
-              position="top-right"
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              transition={Slide}
-              className={"toast-styles"}
-            />
-            <style jsx global>{`
-              html {
-                font-family: ${font.style.fontFamily};
-              }
-            `}</style>
-            <Component {...pageProps} />
-          </PersistGate>
+          <ToastContainer
+            position="top-right"
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            transition={Slide}
+            className={"toast-styles"}
+          />
+          <style jsx global>{`
+            html {
+              font-family: ${font.style.fontFamily};
+            }
+          `}</style>
+          <Component {...pageProps} />
         </Provider>
       </SessionProvider>
     </ErrorBoundary>
