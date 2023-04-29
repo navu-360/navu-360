b618b05846da3cc77a1ad9fde55624a0c82f7f04
Liplan Lekipising <leeplanke@gmail.com>
2023-04-29 13:31:58 +0200

    chore: test build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
@@ -22,12 +18,6 @@ const withPWA = require("next-pwa")({
 const config = withPWA({
   reactStrictMode: true,
   sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
     hideSourceMaps: true,
   },
   images: {
@@ -51,15 +41,7 @@ const config = withPWA({
 });
 
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true,
 };
 
 const modifiedConfig = withSentryConfig(config, sentryWebpackPluginOptions);
