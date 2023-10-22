ba917e1a56f3c8214e2359dc1a0812885dc68042
Liplan Lekipising <leeplanke@gmail.com>
2023-10-22 12:27:47 +0300

    feat: update pricing

--- src/components/landing/pricing.tsx
+++ src/components/landing/pricing.tsx
@@ -103,7 +103,7 @@ export default function Pricing({
                   Starter
                 </h3>
                 <span className="mb-5 text-center text-4xl font-bold">
-                  $25<span className="text-sm">/mo</span>
+                  $30<span className="text-sm">/mo</span>
                 </span>
                 <ul className="my-8 flex flex-col gap-2 text-sm">
                   <li className="flex items-center gap-2 leading-tight">
@@ -134,7 +134,7 @@ export default function Pricing({
                         clipRule="evenodd"
                       />
                     </svg>
-                    Additional seats at $2/month
+                    Additional seats at $3/month
                   </li>
                   <li className="flex items-center gap-2 leading-tight">
                     <svg
@@ -149,7 +149,22 @@ export default function Pricing({
                         clipRule="evenodd"
                       />
                     </svg>
-                    Max 3 courses
+                    Unlimited courses
+                  </li>
+                  <li className="flex items-center gap-2 leading-tight">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="h-6 w-6 shrink-0 text-green-500"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Single Sign On
                   </li>
                   <li className="flex items-center gap-2 leading-tight">
                     <svg
@@ -215,7 +230,7 @@ export default function Pricing({
                   Regular
                 </h3>
                 <span className="mb-5 text-center text-4xl font-bold lg:text-5xl">
-                  $59<span className="text-sm">/mo</span>
+                  $80<span className="text-sm">/mo</span>
                 </span>
                 <ul className="my-8 flex  flex-col gap-2 text-sm">
                   <li className="flex items-center gap-2 leading-tight">
@@ -231,7 +246,7 @@ export default function Pricing({
                         clipRule="evenodd"
                       />
                     </svg>
-                    25 talent seats
+                    30 talent seats
                   </li>
                   <li className="flex items-center gap-2 leading-tight">
                     <svg
@@ -246,7 +261,7 @@ export default function Pricing({
                         clipRule="evenodd"
                       />
                     </svg>
-                    Additional seats at $2/month
+                    Additional seats at $2.5/month
                   </li>
                   <li className="flex items-center gap-2 leading-tight">
                     <svg
@@ -261,7 +276,22 @@ export default function Pricing({
                         clipRule="evenodd"
                       />
                     </svg>
-                    Max 10 courses
+                    Unlimited courses
+                  </li>
+                  <li className="flex items-center gap-2 leading-tight">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="h-6 w-6 shrink-0 text-green-500"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Single Sign On
                   </li>
                   <li className="flex items-center gap-2 leading-tight">
                     <svg
@@ -327,7 +357,7 @@ export default function Pricing({
                   Pro
                 </h3>
                 <span className="mb-5 text-center text-4xl font-bold">
-                  $125<span className="text-sm">/mo</span>
+                  $180<span className="text-sm">/mo</span>
                 </span>
                 <ul className="my-8 flex flex-col gap-2 text-sm">
                   <li className="flex items-center gap-2 leading-tight">
@@ -343,7 +373,7 @@ export default function Pricing({
                         clipRule="evenodd"
                       />
                     </svg>
-                    Unlimited talent courses
+                    200 talent seats
                   </li>
                   <li className="flex items-center gap-2 leading-tight">
                     <svg
@@ -358,7 +388,37 @@ export default function Pricing({
                         clipRule="evenodd"
                       />
                     </svg>
-                    Unlimited templates
+                    Additional seats at $1/month
+                  </li>
+                  <li className="flex items-center gap-2 leading-tight">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="h-6 w-6 shrink-0 text-green-500"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Unlimited courses
+                  </li>
+                  <li className="flex items-center gap-2 leading-tight">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="h-6 w-6 shrink-0 text-green-500"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Single Sign On
                   </li>
                   <li className="flex items-center gap-2 leading-tight">
                     <svg
