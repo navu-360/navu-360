06215bc49f66030b9637d104c7265dc93893e8d5
Liplan Lekipising <leeplanke@gmail.com>
2023-10-27 14:04:14 +0300

    chore: add new side menu option

--- src/components/layout/dashboard.sidebar.tsx
+++ src/components/layout/dashboard.sidebar.tsx
@@ -82,20 +82,45 @@ export default function AdminNav({
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  className=" transition-all duration-300 ease-in md:group-hover:rotate-[-25deg]"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="transition-all duration-300 ease-in md:group-hover:rotate-[-25deg]"
                 >
-                  <path d="m16 6 4 14"></path>
-                  <path d="M12 6v14"></path>
-                  <path d="M8 8v12"></path>
-                  <path d="M4 4v16"></path>
+                  <path d="M2 16V4a2 2 0 0 1 2-2h11" />
+                  <path d="M5 14H4a2 2 0 1 0 0 4h1" />
+                  <path d="M22 18H11a2 2 0 1 0 0 4h11V6H11a2 2 0 0 0-2 2v12" />
                 </svg>
               }
               text={userProfile?.role === "admin" ? "Courses" : "My Courses"}
               isActive={router.pathname.includes("programs")}
               to={"/programs"}
+            />
+          )}
+          {userProfile?.role === "admin" && (
+            <OneItem
+              svg={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="transition-all duration-300 ease-in md:group-hover:rotate-[-25deg]"
+                >
+                  <path d="m16 6 4 14" />
+                  <path d="M12 6v14" />
+                  <path d="M8 8v12" />
+                  <path d="M4 4v16" />
+                </svg>
+              }
+              text={"Library"}
+              isActive={router.pathname.includes("library")}
+              to={"/library"}
             />
           )}
 
