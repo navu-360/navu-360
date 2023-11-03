8ace77971bd571dfafb307224f816b1cbcd1e925
Liplan Lekipising <leeplanke@gmail.com>
2023-11-03 17:31:18 +0300

    chore: disable accelerate

--- src/auth/db.ts
+++ src/auth/db.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client'
+// import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prisma = new PrismaClient().$extends(withAccelerate())
+// export const prisma = new PrismaClient().$extends(withAccelerate())
+export const prisma = new PrismaClient()
