5e7870bb9fdd45fb3d68c0439ceb7dda2e9bf8f4
Liplan Lekipising <leeplanke@gmail.com>
2023-11-12 13:54:34 +0300

    chore: add size limit to avatar

--- src/utils/avatar.ts
+++ src/utils/avatar.ts
@@ -1,3 +1,3 @@
 export const generateAvatar = (seed: string | number) => {
-  return `https://api.dicebear.com/7.x/initials/svg?seed=${seed}&radius=50&backgroundType=gradientLinear&fontFamily=Arial&fontSize=41&fontWeight=500`;
+  return `https://api.dicebear.com/7.x/initials/svg?seed=${seed}&radius=50&backgroundType=gradientLinear&fontFamily=Arial&fontSize=41&fontWeight=500&size=96`;
 };
