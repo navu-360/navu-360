d5ced2c0f9d03f8ac8a89875173ddbdd6b18d606
Liplan Lekipising <leeplanke@gmail.com>
2023-12-11 13:50:32 +0300

    minor updates faq

---
+++ src/components/landing/bgEffect.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function BgEffect({
+  flip,
+  id,
+}: {
+  flip?: boolean;
+  id?: string;
+}) {
+  return (
+    <div className="absolute bottom-0 right-0 z-[-1] h-full w-full">
+      <svg
+        width="1440"
+        height="886"
+        viewBox="0 0 1440 886"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={`${flip ? "rotate-90" : "rotate-0"} `}
+      >
+        <path
+          opacity="0.5"
+          d="M193.307 -273.321L1480.87 1014.24L1121.85 1373.26C1121.85 1373.26 731.745 983.231 478.513 729.927C225.976 477.317 -165.714 85.6993 -165.714 85.6993L193.307 -273.321Z"
+          fill="url(#paint0_linear)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear"
+            x1="1308.65"
+            y1="1142.58"
+            x2="602.827"
+            y2="-418.681"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#3056D3" stop-opacity="0.36" />
+            <stop offset="1" stop-color="#9d2a57" stop-opacity="0" />
+            <stop offset="1" stop-color="#9d2a57" stop-opacity="0.096144" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+}
