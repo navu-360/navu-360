954e941de6db42456146187a17acd4086f72b1bf
Liplan Lekipising <leeplanke@gmail.com>
2023-12-18 20:12:53 +0300

    add vercel speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { makeStore } from "redux/store";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 import Smartlook from "smartlook-client";
 
@@ -77,6 +79,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
           `}</style>
           <Component {...pageProps} />
           <Analytics />
+          <SpeedInsights />
         </Provider>
       </ErrorBoundary>
     </SessionProvider>
