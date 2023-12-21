c9fd375ef752a576e8cebddf72074d92a0693804
Liplan Lekipising <leeplanke@gmail.com>
2023-12-21 21:12:02 +0300

    create learning path UI

--- src/components/createProgram/createLearningPath/main.tsx
+++ src/components/createProgram/createLearningPath/main.tsx
@@ -1,17 +1,30 @@
 import type { OnboardingProgram } from "@prisma/client";
 import Image from "next/image";
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useGetOrganizationProgramsQuery } from "services/baseApiSlice";
 
 export default function CreateLearningPath({ close }: { close: () => void }) {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+
+  const orgId = useSelector(
+    (state: { auth: { orgId: string } }) => state.auth.orgId,
+  );
+
+  const { data: programs } = useGetOrganizationProgramsQuery(orgId, {
+    skip: !orgId,
+    refetchOnMountOrArgChange: true,
+  });
+
+  const [selectedCourses, setSelectedCourses] = useState<string[]>([]);
 
   return (
     <div
       onClick={(e) => (e.currentTarget === e.target ? close() : null)}
       className="fixed inset-0 z-[200] flex h-screen w-screen items-center justify-center bg-dark/30 backdrop-blur-sm"
     >
-      <div className="relative h-[80vh] min-h-[600px] w-[1000px] rounded-lg bg-white p-4 px-8">
+      <div className="relative h-[80vh] min-h-[600px] w-[1000px] overflow-hidden rounded-lg bg-white p-4 px-8">
         <div
           onClick={() => close()}
           className="absolute -right-4 -top-12 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gray-50"
@@ -35,7 +48,7 @@ export default function CreateLearningPath({ close }: { close: () => void }) {
         <h2 className="text-xl font-semibold capitalize text-dark">
           Create Learning Path
         </h2>
-        <form className="mt-8 flex h-full w-full flex-col gap-12 overflow-y-auto">
+        <form className="no-scrollbar mt-8 flex h-full w-full flex-col gap-12 overflow-y-auto">
           <div className="relative flex flex-col gap-2">
             <label htmlFor="role">Path Name</label>
             <input
@@ -78,25 +91,106 @@ export default function CreateLearningPath({ close }: { close: () => void }) {
             <h3 className="text-base font-semibold text-tertiary">
               Choose Courses
             </h3>
-            <div className="grid grid-cols-4">
-              <CourseCard />
+            <div className="mt-2 grid h-full flex-1 grid-cols-5 gap-4 overflow-y-auto pb-32">
+              {programs?.data?.map((course: OnboardingProgram) => (
+                <CourseCard
+                  course={course}
+                  key={course?.id}
+                  isSelected={selectedCourses.includes(course?.id)}
+                  toggleCourse={() => {
+                    if (selectedCourses.includes(course?.id)) {
+                      setSelectedCourses(
+                        selectedCourses.filter((id) => id !== course?.id),
+                      );
+                    } else {
+                      setSelectedCourses([...selectedCourses, course?.id]);
+                    }
+                  }}
+                />
+              ))}
             </div>
           </section>
         </form>
+
+        <div className="absolute inset-x-0 bottom-0 z-20 flex w-full flex-row-reverse items-center justify-between gap-2 bg-white px-8 py-4 md:gap-8">
+          <button
+            disabled={
+              name.length === 0 ||
+              description.length === 0 ||
+              selectedCourses.length === 0
+            }
+            className="h-max w-full rounded-md bg-secondary px-8 py-1.5 text-sm font-semibold text-white md:w-max"
+          >
+            Create Learning Path
+          </button>
+          <button className="rounded-md border-[1px] border-gray-400 bg-transparent px-8 py-1.5 text-sm font-medium text-gray-500">
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
-function CourseCard({ course }: { course: OnboardingProgram }) {
+function CourseCard({
+  course,
+  isSelected,
+  toggleCourse,
+}: {
+  course: OnboardingProgram;
+  isSelected: boolean;
+  toggleCourse: () => void;
+}) {
   return (
-    <div className="relative h-[300px] w-[300px] rounded-lg bg-white">
-      <Image
-        src="https://res.cloudinary.com/dpnbddror/image/upload/v1678044671/Rectangle_417_1_1_pq5jum.png"
-        alt={course?.name}
-        fill
-        className="rounded-lg"
-      />
+    <div
+      onClick={() => toggleCourse()}
+      className="shadowAroundFeature relative h-[150px] w-full rounded-md bg-white"
+    >
+      {isSelected ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-check-circle-2 absolute right-2 top-2 z-20 text-green-500"
+        >
+          <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
+          <path d="m9 12 2 2 4-4" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-circle absolute right-2 top-2 z-20 text-gray-300"
+        >
+          <circle cx="12" cy="12" r="10" />
+        </svg>
+      )}
+      <div className="relative z-10 h-full w-full rounded-md">
+        <Image
+          src={course?.image ?? ""}
+          alt={course?.name}
+          fill
+          className="h-full w-full rounded-md object-cover"
+        />
+      </div>
+      <div className="overlay-dark absolute inset-x-0 bottom-0 z-[15] flex h-full items-end !rounded-md pb-2 text-white">
+        <h4 className="px-2 text-sm font-semibold text-white">
+          {course?.name}
+        </h4>
+      </div>
     </div>
   );
 }
