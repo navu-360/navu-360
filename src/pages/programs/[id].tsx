aaeab775826de138c051fed9595d463f28d04ac9
Liplan Lekipising <leeplanke@gmail.com>
2024-01-05 20:35:36 +0300

    update course details

--- src/pages/programs/[id].tsx
+++ src/pages/programs/[id].tsx
@@ -258,7 +258,10 @@ export default function Program({
                     <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
                   </svg>
                   <span className="text-xs font-medium">
-                    Total Chapters {data?.ProgramSection?.length || 0}
+                    Total Chapters{" "}
+                    <span className="font-medium">
+                      {data?.ProgramSection?.length || 0}
+                    </span>
                   </span>
                 </div>
                 <div className="flex items-center gap-2 px-2">
@@ -281,7 +284,10 @@ export default function Program({
                     <path d="M13 18h8" />
                   </svg>
                   <span className="text-xs font-medium">
-                    Total Questions {data?.QuizQuestion?.length || 0}
+                    Total Questions{" "}
+                    <span className="font-medium">
+                      {data?.QuizQuestion?.length || 0}
+                    </span>
                   </span>
                 </div>
               </div>
@@ -304,7 +310,12 @@ export default function Program({
                     d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
                   />
                 </svg>
-                <p>{enrolledTalents?.data?.length || 0} talents enrolled</p>
+                <p>
+                  Talents enrolled{" "}
+                  <span className="font-medium">
+                    {enrolledTalents?.data?.length || 0}
+                  </span>
+                </p>
               </div>
               {/* edit, delete */}
               <div className="mt-4 flex justify-start gap-4 px-4">
